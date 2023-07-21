bbba0701e9b467898752e05b0043a030fce3f8f7
Maksim Tyunun <41119975+KubrickRubrik@users.noreply.github.com>
2023-07-21 16:34:25 +0300

    Feature/managers (#3)
    * Added database Drift
    
    1. Added database tables [UseTableAppUser], [UseTableAppSettings],
       [UseTableSeries], [UseTableScenes].
    2. Added сonfiguration of database connection [ConnectDataBase]
    3. Added migration of database connection [MigrationDataBaseDrift]
    4. Added classes of querials to the database [AppQueryDrift],
       [SeriesQueryDrift], [SceneQueryDrift]
    
    * Added pages state managers
    
    1. Added a request for authorization in [SessionProvider]
    2. Added pages added [PageControllerProvider]
    3. Added state managers [SeriesProvider] and [ScenesProvider]
    
    * Updated locator
    
    1. Added api database
    
    * Fix locator

--- lib/presentation/locator/locator.dart
+++ lib/presentation/locator/locator.dart
@@ -1,7 +1,13 @@
 import 'package:facetomini/data/api/core/database/api.dart';
 import 'package:facetomini/data/api/interfaces/api_db.dart';
-import 'package:get_it/get_it.dart';
+import 'package:facetomini/data/repository/authorization.dart';
+import 'package:facetomini/domain/repository/authorization.dart';
+import 'package:facetomini/domain/use_cases/authorization.dart';
+import 'package:facetomini/presentation/manager/pages/a_home/controller/controller.dart';
+import 'package:facetomini/presentation/manager/pages/a_home/tab_1_series/series.dart';
+import 'package:facetomini/presentation/manager/pages/a_home/tab_2.scenes/scenes.dart';
 import 'package:facetomini/presentation/manager/session/session.dart';
+import 'package:get_it/get_it.dart';
 
 final locator = GetIt.I;
 
@@ -10,9 +16,14 @@ abstract final class ServicesLocator {
     // API
     locator.registerLazySingleton<ApiDbEnvelope>(() => ApiDbDrift());
     // Repository
+    locator.registerLazySingleton<AuthorizationRepository>(() => AuthorizationRepositoryImpl(locator()));
     // Use Cases
+    locator.registerLazySingleton<AuthorizationCase>(() => AuthorizationCase(locator()));
     // Manager
-    locator.registerLazySingleton<SessionProvider>(() => SessionProvider());
+    locator.registerLazySingleton<SessionProvider>(() => SessionProvider(locator()));
     // Pages
+    locator.registerLazySingleton(() => PageControllerProvider());
+    locator.registerLazySingleton(() => SeriesProvider());
+    locator.registerLazySingleton(() => ScenesProvider());
   }
 }
