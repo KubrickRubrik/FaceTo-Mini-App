790bb4685e325c726a6feecc954e95c890bb63f1
Maksym Tyunin <41119975+KubrickRubrik@users.noreply.github.com>
2023-07-22 13:52:23 +0300

    Added requests to change application settings in [SessionQueryDrift]
    1. [setTheme], [setLocale], [setSound]

--- lib/data/api/core/database/queries/session.dart
+++ lib/data/api/core/database/queries/session.dart
@@ -23,19 +23,28 @@ final class SessionQueryDrift {
 
   // Save a theme
   Future<TableAppSettings?> setTheme(ConnectDataBase db, {required String theme}) async {
-    final userTable = db.alias(db.useTableAppUser, 'user');
-    return null;
+    final query = (db.update(db.useTableAppSettings)..where((tbl) => tbl.id.equals(1)));
+    final response = await query.writeReturning(UseTableAppSettingsCompanion(theme: Value(theme)));
+    print(response);
+    if (response.isEmpty) return null;
+    return response.first;
   }
 
   // Locale saving
   Future<TableAppSettings?> setLocale(ConnectDataBase db, {required String language}) async {
-    final userTable = db.alias(db.useTableAppUser, 'user');
-    return null;
+    final query = (db.update(db.useTableAppSettings)..where((tbl) => tbl.id.equals(1)));
+    final response = await query.writeReturning(UseTableAppSettingsCompanion(language: Value(language)));
+    print(response);
+    if (response.isEmpty) return null;
+    return response.first;
   }
 
   // Saving sound
   Future<TableAppSettings?> setSound(ConnectDataBase db, {required bool enebledSound}) async {
-    final userTable = db.alias(db.useTableAppUser, 'user');
-    return null;
+    final query = (db.update(db.useTableAppSettings)..where((tbl) => tbl.id.equals(1)));
+    final response = await query.writeReturning(UseTableAppSettingsCompanion(enabledSound: Value(enebledSound ? 1 : 0)));
+    print(response);
+    if (response.isEmpty) return null;
+    return response.first;
   }
 }
--- lib/data/mapper/app.dart
+++ lib/data/mapper/app.dart
@@ -4,10 +4,20 @@ import 'package:facetomini/domain/entities/app.dart';
 /// Transformation of the 'raw' models in the entities used
 abstract final class EntitiesMapper {
   //
-  static SessionEntity? authorizedMapper(SessionModel? model) {
+  static SessionEntity? setAuthorized(SessionModel? model) {
     if (model == null) return null;
     return SessionEntity(
       idApp: model.idApp,
+      enabledSound: model.enabledSound,
+      locale: model.language,
+      theme: model.theme,
+    );
+  }
+
+  static SessionEntity? setSettings(SessionModel? model) {
+    if (model == null) return null;
+    return SessionEntity(
+      idApp: 0,
       enabledSound: model.enabledSound,
       locale: model.language,
       theme: model.theme,
